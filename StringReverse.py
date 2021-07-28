b52d18421eebcad46b10afd8e9418f9505d89a6d
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-28 19:42:42 +0200

    Update StringReverse.py

--- StringReverse.py
+++ StringReverse.py
@@ -1,3 +1,10 @@
 def stringWordReverse(s):
     return s.split()[::-1]
 print(" ".join(stringWordReverse("Hello world")))
+
+def stringReverse(s):
+    r = ""
+    for i in s:
+        r = i + r
+    return r
+print(stringReverse("Hello world"))
