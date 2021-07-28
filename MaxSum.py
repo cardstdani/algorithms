ce2845c006230e64b85f8ed6602eda6fb49a6a61
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-28 19:22:54 +0200

    Update MaxSum.py

--- MaxSum.py
+++ MaxSum.py
@@ -11,6 +11,13 @@ def maxSum(list):
                 if (a+i) > currentSum:
                     currentSum = a+i
     return currentSum
-        
+
+def recursiveMaxSum(list, currentSum=0):
+    if len(list) <= 1:
+        return currentSum
+    for i in list:
+        if (list[0]+i) > currentSum:
+            currentSum = list[0]+i
+    return recursiveMaxSum(list[1:], currentSum)
     
 print(maxSum([1, 2, 3, 4, 5]))
