29f2f17af3ae077f12ecdde19963b69022e664e3
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-28 19:14:20 +0200

    Update MaxSum.py

--- MaxSum.py
+++ MaxSum.py
@@ -3,13 +3,14 @@ def maxSum(list):
         return None
     
     s = []
-    sums = []
+    currentSum = 0
     for i in list:
         s.append(i)
         for a in list:
             if not a in s:
-                sums.append(a+i)
-    return max(sums)
+                if (a+i) > currentSum:
+                    currentSum = a+i
+    return currentSum
         
     
 print(maxSum([1, 2, 3, 4, 5]))
