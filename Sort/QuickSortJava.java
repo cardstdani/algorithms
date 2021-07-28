93cf031667e79c63e81f52aa5c7039a6e77502ec
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-28 11:59:37 +0200

    Create QuickSortJava.java

---
+++ Sort/QuickSortJava.java
@@ -0,0 +1,36 @@
+import java.lang.Math;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Integer> myList = new ArrayList<>();
+		for (int i = 0; i < 15; i++) {
+			myList.add(new Random().nextInt(40));
+		}
+		System.out.println(myList);
+		System.out.println(quickSort(myList));
+	}
+
+	public static List<Integer> quickSort(List<Integer> list) {
+		if (list.size() <= 1) {
+			return list;
+		}
+		int pivot = 0;
+		List<Integer> left = new ArrayList<>();
+		List<Integer> right = new ArrayList<>();
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i) < list.get(pivot)) {
+				left.add(list.get(i));
+			} else {
+				right.add(list.get(i));
+			}
+		}
+
+		List<Integer> joinedList = quickSort(left);
+		joinedList.add(list.get(pivot));
+		joinedList.addAll(quickSort(right));
+		return joinedList;
+	}
+}
