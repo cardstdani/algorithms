667357a7c4bd11912888369620571035f999b94c
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-07-29 19:34:45 +0200

    Create CanConstruct.py

---
+++ RecursiveFunctions/CanConstruct.py
@@ -0,0 +1,35 @@
+def canConstruct(target, list):
+    if len(target) == 0:
+        return True
+
+    for i in list:
+        for a in range(len(i)):
+            if not target[a] == i[a]:
+                break
+        newTarget = target[len(i):]
+        if canConstruct(newTarget, list) == True:
+            return True
+    return False
+
+def canConstructMem(target, list, mem={}):
+    if target in mem:
+        return mem[target]
+    if len(target) == 0:
+        return True
+
+    for i in list:
+        for a in range(len(i)):
+            if not target[a] == i[a]:
+                break
+        newTarget = target[len(i):]
+        if canConstructMem(newTarget, list, mem) == True:
+            mem[target] = True
+            return True
+    mem[target] = False
+    return mem[target]
+
+
+print(canConstruct("abcdef", ["a", "b", "c", "d", "e", "f"]))
+print(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
+print(canConstructMem("abcdef", ["a", "b", "c", "d", "e", "f"]))
+print(canConstructMem("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
