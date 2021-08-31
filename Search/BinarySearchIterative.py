adda539c731c7cee4d329624edf7892b75fdffed
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-08-31 18:47:24 +0200

    Update BinarySearchIterative.py

--- Search/BinarySearchIterative.py
+++ Search/BinarySearchIterative.py
@@ -1,16 +1,17 @@
 def binarySearch(list, target):
   start = 0
+  mid = 0
   end = len(list)
   while start < end:
-    mid = (end - start)//2
+    mid = (end + start)//2
     if list[mid] == target:
       return mid
     elif (end-start) <= 1:
       return None
     elif list[mid] > target:
-      end = end//2
+      end = mid
     elif list[mid] < target:
-      start = end//2    
+      start = mid
   return None
 
-print(binarySearch([0, 1, 2, 3, 4, 5, 6], 2))
+print(binarySearch([0, 1, 2, 3, 4, 5, 6], 4))
